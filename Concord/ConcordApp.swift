484305a0d5ce5efd1812f2e04710cc16864c544b
Remy Ochei <do.infinitely@gmail.com>
2025-10-20 17:52:52 -0500

    first commit

---
+++ Concord/ConcordApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConcordApp.swift
+//  Concord
+//
+//  Created by Remy Ochei on 10/20/25.
+//
+
+import SwiftUI
+
+@main
+struct ConcordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
